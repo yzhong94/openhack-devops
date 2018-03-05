d06f0abb601e8cb15ae787ab674fd5ac88531a86
Ricardo Serradas <riserrad@microsoft.com>
2018-03-05 14:43:24 -0800

    Adding new class library to store new Data Objects without Entity Framework dependency

---
+++ src/MobileAppService/MyDriving.ServiceObjects/BaseDataObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDriving.ServiceObjects
+{
+    public class BaseDataObject
+    {
+        public string Id;
+
+        public BaseDataObject()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+    }
+}
