82a6886c21b6830978e61c546668d1cd6f76582b
Osvaldo Daibert <odaibert@microsoft.com>
2018-03-06 16:23:06 -0800

    Refactored to use DbContext

--- src/MobileAppService/POIService-DataInsert/GeneratePOIData.cs
+++ src/MobileAppService/POIService-DataInsert/GeneratePOIData.cs
@@ -13,49 +13,56 @@ namespace MyDriving.POIService.v1
         [FunctionName("GeneratePOIData")]
         public static void Run([TimerTrigger("*/1 * * * * *")]TimerInfo myTimer, TraceWriter log)
         {
-            //log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
-
             string TripId = Guid.NewGuid().ToString();
 
-            string _connectionString = "Server=tcp:mydrivingdbserver-fxw5u47lzepqy.database.windows.net,1433;Initial Catalog=mydrivingDB;Persist Security Info=False;User ID=YourUserName;Password=@windowsPhone10;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
-            using (SqlConnection conn = new SqlConnection(_connectionString))
+            using (var context = new MyDrivingContext())
             {
-                conn.Open();
-
-                for (int i = 0; i < 20; i++)
+                for (int i = 0; i < 5; i++)
                 {
-                    using (SqlCommand cmd = new SqlCommand(GetSQLCommand(TripId), conn))
-                    {
-                        log.Info($"{cmd.ExecuteNonQuery()} rows were updated");
-                    }
-                }                
+                    context.POIs.Add(
+                        new POI
+                        {
+                            TripId = TripId,
+                            Latitude = GetLatitude(516400146, 630304598),
+                            Longitude = GetLongitude(224464416, 341194152),
+                            POIType = POIType.HardAcceleration,
+                            Timestamp = DateTime.Now,
+                            Deleted = false
+                        });
+
+                    context.SaveChanges();
+
+                    log.Info($"TripId {TripId} saved");
+
+                }
+               
             }
         }
-        
-        private static string GetSQLCommand(string TripId)
-        {
-            return "DECLARE @datetimeoffset datetimeoffset(7) = '" + DateTime.Now.ToString() + "'" +
-                "INSERT INTO [dbo].[POIs]" +
-                       "([TripId]" +
-                       ",[Latitude]" +
-                       ",[Longitude]" +
-                       ",[POIType]" +
-                       ",[RecordedTimeStamp]" +
-                       ",[CreatedAt]" +
-                       ",[UpdatedAt]" +
-                       ",[Deleted]" +
-                       ",[Timestamp])" +
-                         "VALUES('" + TripId.ToString() + "'" +
-                         ", " + GetLatitude(516400146, 630304598) +
-                          "," + GetLongitude(224464416, 341194152) +
-                         ", 1" +
-                         ", '" + DateTime.Now.ToString() + "'" +
-                         ", '" + DateTime.Now.ToString() + "'" +
-                         ", @datetimeoffset" +
-                         ", 0" +
-                         ", '" + DateTime.Now.ToString() + "')";
-        }
+
+
+        //private static string GetSQLCommand(string TripId)
+        //{
+        //    return "DECLARE @datetimeoffset datetimeoffset(7) = '" + DateTime.Now.ToString() + "'" +
+        //        "INSERT INTO [dbo].[POIs]" +
+        //               "([TripId]" +
+        //               ",[Latitude]" +
+        //               ",[Longitude]" +
+        //               ",[POIType]" +
+        //               ",[RecordedTimeStamp]" +
+        //               ",[CreatedAt]" +
+        //               ",[UpdatedAt]" +
+        //               ",[Deleted]" +
+        //               ",[Timestamp])" +
+        //                 "VALUES('" + TripId.ToString() + "'" +
+        //                 ", " + GetLatitude(516400146, 630304598) +
+        //                  "," + GetLongitude(224464416, 341194152) +
+        //                 ", 1" +
+        //                 ", '" + DateTime.Now.ToString() + "'" +
+        //                 ", '" + DateTime.Now.ToString() + "'" +
+        //                 ", @datetimeoffset" +
+        //                 ", 0" +
+        //                 ", '" + DateTime.Now.ToString() + "')";
+        //}
 
         #region CORDINATES
         private static int GetLatitude(int From, int To)
