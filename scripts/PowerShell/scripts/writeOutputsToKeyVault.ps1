a991cbe5779715ea349e4f8623f2523f5ae4e7f2
Oguz Pastirmaci <oguzp@Oguzs-MacBook-Pro.local>
2018-04-09 15:43:51 -0700

    Removed unnecessary lines

--- scripts/PowerShell/scripts/writeOutputsToKeyVault.ps1
+++ scripts/PowerShell/scripts/writeOutputsToKeyVault.ps1
@@ -1,7 +1,3 @@
-$sqlServerFullyQualifiedDomainName = $deployment2.Outputs.sqlServerFullyQualifiedDomainName.Value
-$sqlServerAdminLogin = $deployment2.Outputs.sqlServerAdminLogin.Value
-$sqlDBName = $deployment2.Outputs.sqlDBName.Value
-
 Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlServerFullyQualifiedDomainName' -SecretValue (ConvertTo-SecureString $sqlServerFullyQualifiedDomainName -AsPlainText -Force)
 Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlServerAdminLogin' -SecretValue (ConvertTo-SecureString $sqlServerAdminLogin -AsPlainText -Force)
 Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlDBName' -SecretValue (ConvertTo-SecureString $sqlDBName -AsPlainText -Force)
