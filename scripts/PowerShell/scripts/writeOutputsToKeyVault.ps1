8d4e79fe5c6b71621eeaec34f11535a27dcdfcf3
Oguz Pastirmaci <oguzp@surfhpcen03n31.redmond.corp.microsoft.com>
2018-04-09 18:10:47 -0700

    Troubleshooting null string error

--- scripts/PowerShell/scripts/writeOutputsToKeyVault.ps1
+++ scripts/PowerShell/scripts/writeOutputsToKeyVault.ps1
@@ -1,11 +1,16 @@
 # MyDriving DB
 Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlServerFullyQualifiedDomainName' -SecretValue (ConvertTo-SecureString $sqlServerFullyQualifiedDomainName -AsPlainText -Force)
+Write-Host "1"
 Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlServerAdminLogin' -SecretValue (ConvertTo-SecureString $sqlServerAdminLogin -AsPlainText -Force)
+Write-Host "2"
 Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlDBName' -SecretValue (ConvertTo-SecureString $sqlDBName -AsPlainText -Force)
+Write-Host "3"
+
 
 # MyDriving Analytics DB
-
 Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlAnalyticsFullyQualifiedDomainName' -SecretValue (ConvertTo-SecureString $sqlAnalyticsFullyQualifiedDomainName -AsPlainText -Force)
+Write-Host "4"
 Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlAnalyticsServerAdminLogin' -SecretValue (ConvertTo-SecureString $sqlAnalyticsServerAdminLogin -AsPlainText -Force)
+Write-Host "5"
 Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlAnalyticsDBName' -SecretValue (ConvertTo-SecureString $sqlAnalyticsDBName -AsPlainText -Force)
-
+Write-Host "6"
