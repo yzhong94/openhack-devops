11162292b44dd079e34404b1b3a18660d44ad79c
Oguz Pastirmaci <oguzp@surfhpcen03n31.redmond.corp.microsoft.com>
2018-04-09 18:35:00 -0700

    Fixing variables

--- scripts/PowerShell/scripts/writeOutputsToKeyVault.ps1
+++ scripts/PowerShell/scripts/writeOutputsToKeyVault.ps1
@@ -1,15 +1,9 @@
 # MyDriving DB
-Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlServerFullyQualifiedDomainName' -SecretValue (ConvertTo-SecureString $sqlServerFullyQualifiedDomainName -AsPlainText -Force)
-Write-Host "1"
-Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlServerAdminLogin' -SecretValue (ConvertTo-SecureString $sqlServerAdminLogin -AsPlainText -Force)
-Write-Host "2"
-Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlDBName' -SecretValue (ConvertTo-SecureString $sqlDBName -AsPlainText -Force)
-Write-Host "3"
+Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlServerFullyQualifiedDomainName' -SecretValue (ConvertTo-SecureString $(sqlServerFullyQualifiedDomainName) -AsPlainText -Force)
+Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlServerAdminLogin' -SecretValue (ConvertTo-SecureString $(sqlServerAdminLogin) -AsPlainText -Force)
+Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlDBName' -SecretValue (ConvertTo-SecureString $(sqlDBName) -AsPlainText -Force)
 
 # MyDriving Analytics DB
-Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlAnalyticsFullyQualifiedDomainName' -SecretValue (ConvertTo-SecureString $sqlAnalyticsFullyQualifiedDomainName -AsPlainText -Force)
-Write-Host "4"
-Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlAnalyticsServerAdminLogin' -SecretValue (ConvertTo-SecureString $sqlAnalyticsServerAdminLogin -AsPlainText -Force)
-Write-Host "5"
-Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlAnalyticsDBName' -SecretValue (ConvertTo-SecureString $sqlAnalyticsDBName -AsPlainText -Force)
-Write-Host "6"
+Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlAnalyticsFullyQualifiedDomainName' -SecretValue (ConvertTo-SecureString $(sqlAnalyticsFullyQualifiedDomainName) -AsPlainText -Force)
+Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlAnalyticsServerAdminLogin' -SecretValue (ConvertTo-SecureString $(sqlAnalyticsServerAdminLogin) -AsPlainText -Force)
+Set-AzureKeyVaultSecret -VaultName $keyVaultName -Name 'sqlAnalyticsDBName' -SecretValue (ConvertTo-SecureString $(sqlAnalyticsDBName) -AsPlainText -Force)
