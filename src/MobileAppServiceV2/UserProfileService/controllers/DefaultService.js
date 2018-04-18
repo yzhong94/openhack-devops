7e2a768717e1d17c3bc26057786f0926a7b35f18
David Tesar <dtzar@hotmail.com>
2018-04-17 21:29:52 -0700

    Tedious refactor

--- src/MobileAppServiceV2/UserProfileService/controllers/DefaultService.js
+++ src/MobileAppServiceV2/UserProfileService/controllers/DefaultService.js
@@ -1,77 +1,58 @@
-'use strict';
+//'use strict';
 
 //const sql = require('mssql')
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 var config = require('../config/config');
-var getresult;
+
+// https://docs.microsoft.com/en-us/azure/sql-database/sql-database-connect-query-nodejs
+// https://github.com/tediousjs/tedious/blob/master/examples/minimal.js
+var connection = new Connection(config);
+
+connection.on('connect', function(err) 
+   {
+     if (err) 
+       {
+          console.log(err)
+       }
+    else
+       {
+          console.log("connection working")
+       }
+   }
+ );
+
+function queryDatabase(sqlquery){ 
+  console.log('Reading rows from the Table...');
+
+  // Read all rows from table
+  request = new Request(
+    sqlquery,
+      function(err, rowCount, rows) {
+        console.log(rowCount + ' row(s) returned');
+      }
+  );
+
+  request.on('row', function(columns) {
+    columns.forEach(function(column) {
+      console.log("%s\t%s", column.metadata.colName, column.value);
+      var tempobj = column.metadata.value;
+      return tempobj;
+    });
+  });
+  connection.execSql(request);
+}
 
 exports.userProfileGET = function(args, res, next) {
 
-  // https://docs.microsoft.com/en-us/azure/sql-database/sql-database-connect-query-nodejs
-  //https://github.com/tediousjs/tedious/blob/master/examples/minimal.js
-
-  var connection = new Connection(config);
-
-  // Attempt to connect and execute queries if connection goes through
-  connection.on('connect', function(err) 
-     {
-       if (err) 
-         {
-            console.log(err)
-         }
-      else
-         {
-             queryDatabase()
-         }
-     }
-   );
-  
-  function queryDatabase()
-     { console.log('Reading rows from the Table...');
-
-       request = new Request(
-            "select * from UserProfiles"
-              );
-      connection.execSql(request);
-      // Need to get the result back to a meaningful object 
-      // console.log(request)
-      // res.end(JSON.stringify(request))
-     }
-
-    // http://www.dotnetcurry.com/nodejs/1238/connect-sql-server-nodejs-mssql-package - Step 5.  Doesn't work with initial connection, guessing it is because it used the 3.x mssql package (now at 4.x) 
-
-    //  function getUsers() {
-    //   var dbConn = new sql.Connection(config);
-    //   dbConn.connect().then(function () {
-    //       var request = new sql.Request(dbConn);
-    //       request.query("select * from UserProfiles").then(function (recordSet) {
-    //           console.log(recordSet);
-    //           dbConn.close();
-    //       }).catch(function (err) {
-    //           console.log(err);
-    //           dbConn.close();
-    //       });
-    //   }).catch(function (err) {
-    //       console.log(err);
-    //   });
-    // }
-
-  //https://www.npmjs.com/package/mssql#connection-pools
-  // Doesn't actually execute the sql.close() so 2nd request fails.
-
-  // sql.connect(config).then(pool => {
-  //   // Query
-  //   return pool.request()
-  //   //.input('input_parameter', sql.Int, value)
-  //   .query('select * from UserProfiles') 
-  //   //where Id = ')
-  //   }).then(result => {
-  //       console.log(req)
-  //       res.end(JSON.stringify(result)).then(sql.close())
-  //     }).catch(err => {
-  //     // ... error checks
-  //     })
+  var queryString = "SELECT * FROM UserProfiles";
+  var examples = queryDatabase(queryString);
+  if (Object.keys(examples).length > 0) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(examples));
+  } else {
+    res.end();
+  }
 
 }
 
